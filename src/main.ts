99432cfc072e5357be8a8f0d54d182746d820b74
matheuslanduci <matheuslanduci@gmail.com>
2025-08-02 15:56:22 -0300

    fix build stuff

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,28 @@
-import { Client, dirname, load } from 'sunar'
+import { Client, load } from 'sunar'
 import { env } from './env'
 
-const components = ['commands', 'signals', 'autocompletes', 'buttons']
+const components = [
+	'commands',
+	'signals',
+	'autocompletes',
+	'buttons',
+	'modals',
+	'context-menus',
+	'groups',
+	'protectors',
+	'select-menus'
+]
 
 async function main() {
 	const client = new Client({
 		intents: []
 	})
 
-	await load(
-		`${dirname(import.meta.url)}/{${components.join(',')}}/**/*.{js.ts}`
-	)
+	const mainFolder = process.env.NODE_ENV === 'production' ? 'dist' : 'src'
 
-	await client.login(env.DISCORD_BOT_TOKEN)
+	await load(`${mainFolder}/{${components.join(',')}}/**/*.{js,ts}`)
+
+	return client.login(env.DISCORD_BOT_TOKEN)
 }
 
 main().catch(console.error)
